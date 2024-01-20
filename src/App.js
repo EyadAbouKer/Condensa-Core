8ccc56dc25488538613b809914bed5df52e35b10
EyadAbouKer <126291554+EyadAbouKer@users.noreply.github.com>
2024-01-20 14:42:04 -0500

    modified:   .gitignore 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Components/media/media/copy-icon.svg 	new file:   src/Components/media/media/v1.png

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/js/dist/dropdown.js";
+import Header from "./components/Main-WebPage/Header";
+import Body from "./components/Main-WebPage/Body";
 
 function App() {
   return (
     <div className="App">
-      HELLO
+      <Header />
+      <Body />
     </div>
   );
 }
