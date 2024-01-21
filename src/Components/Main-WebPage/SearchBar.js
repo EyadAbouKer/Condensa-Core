5815cca55fb801fd2f9f157aa4fd5b7903afa585
EyadAbouKer <126291554+EyadAbouKer@users.noreply.github.com>
2024-01-20 22:38:05 -0500

    new file:   src/Components/Main-WebPage/Body.css 	new file:   src/Components/Main-WebPage/SearchBar.css 	new file:   src/Components/Main-WebPage/SearchBar.js

---
+++ src/Components/Main-WebPage/SearchBar.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+import SubmitButton from "./SubmitButton";
+
+const SearchBar = ({ URL, setterURL }) => {
+  return (
+    <div className="pt-5 pb-4">
+      <input
+        className="form-control mr-sm-2 bar-background-grey text-white custom-placeholder"
+        type="text"
+        placeholder="Enter Youtube URL"
+        aria-label="Search"
+        onChange={setterURL}
+        value={URL}
+        style={{ border: "1px solid #6F6F6F" }}
+      ></input>
+    </div>
+  );
+};
+
+export default SearchBar;
+// this is header after fixing the problems
